724029c77675ed56ea38b183682a8f16ad250423
Srimanth Mahadev Kancharla <78967903+mahadev521@users.noreply.github.com>
2022-01-04 22:13:38 +0530

    Update tictactoe_2players.py

--- tictactoe_2players.py
+++ tictactoe_2players.py
@@ -1,46 +1,67 @@
-'''general method of playing tictactoe by 2 players'''
+'''general 2 player tic tac toe game'''
 import numpy as np
-x,c=[' ']*9,0
-def win(x):
-    x=np.resize(np.array(x),(3,3))
-    for i in range(3):
-            j=list(set(x[:,i])) #checking vertically
+gc=0 #gamecount
+def game(gc):
+    x,c=[' ']*9,0
+    def win(x): #checking for winning
+        x=np.resize(np.array(x),(3,3))
+        for i in range(3):
+                j=list(set(x[:,i])) #checking vertically
+                if len(j)==1 and j[0]!=' ':
+                    if j[0]=='X': return 'player 1 won'
+                    else: return 'player 2 won'
+                j=list(set(x[i,:])) #checking horizontally
+                if len(j)==1 and j[0]!=' ':
+                    if j[0]=='X': return 'player 1 won'
+                    else: return 'player 2 won'
+        else: #checking diagonals
+            j=list(set(np.diag(x[::-1])))
             if len(j)==1 and j[0]!=' ':
                 if j[0]=='X': return 'player 1 won'
                 else: return 'player 2 won'
-            j=list(set(x[i,:])) #checking horizontally
+            j=list(set(np.diag(x)))
             if len(j)==1 and j[0]!=' ':
                 if j[0]=='X': return 'player 1 won'
                 else: return 'player 2 won'
-    else: #checking diagonals
-        j=list(set(np.diag(x[::-1])))
-        if len(j)==1 and j[0]!=' ':
-            if j[0]=='X': return 'player 1 won'
-            else: return 'player 2 won'
-        j=list(set(np.diag(x)))
-        if len(j)==1 and j[0]!=' ':
-            if j[0]=='X': return 'player 1 won'
-            else: return 'player 2 won'
-    return ''
+        return ''
 
-def display():
-    for i in range(0,9,3): print(x[i:i+3])
+    def display():
+        for i in range(0,9,3): print(x[i:i+3])
 
-while True:
-    i=int(input('player 1\'s turn(\'X\'): '))-1
-    while x[i]!=' ': i=i=int(input('position occupied! enter another position number: '))-1
-    x[i],c='X',c+1
-    display()
-    if c>4 and win(x):
-        print(win(x))
-        break
-    if c==9:
-        print('It\'s a Tie')
-        break
-    i=int(input('player 2\'s turn(\'O\'): '))-1
-    while x[i]!=' ': i=i=int(input('position occupied! enter another position number: '))-1
-    x[i],c='O',c+1
-    display()
-    if c>4 and win(x):
-        print(win(x))
-        break
+    def p1(): #player1 function
+        i=int(input('player 1\'s turn: '))-1
+        while x[i]!=' ': i=i=int(input('position occupied! enter another position number: '))-1
+        x[i]='X' if gc%2==0 else 'O'
+
+    def p2(): #player2 function
+        i=int(input('player 2\'s turn: '))-1
+        while x[i]!=' ': i=i=int(input('position occupied! enter another position number: '))-1
+        x[i]='O' if gc%2==0 else 'X'
+
+    while True:
+        p1() if gc%2==0 else p2() #roles will be reversed in next iteration
+        c+=1
+        display()
+        if c>4 and win(x):
+            if gc%2==0: print(win(x),' won')
+            else:
+                x='player 1' if win(x)=='player 2' else 'player 2'
+                print(x,' won')
+            break
+        if c==9:
+            print('It\'s a Tie')
+            break
+        p2() if gc%2==0 else p1()
+        c+=1
+        display()
+        if c>4 and win(x):
+            if gc%2==0: print(win(x),' won')
+            else:
+                x='player 1' if win(x)=='player 2' else 'player 2'
+                print(x,' won')
+            break
+    ch=input('want to play again(y/n)? ')
+    if ch=='y' or ch=='Y':
+        gc+=1
+        game(gc)
+game(gc)
